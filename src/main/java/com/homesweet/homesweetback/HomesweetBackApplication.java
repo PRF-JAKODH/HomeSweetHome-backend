fbfc8e0773f12dca30c393d443de7e571c0fa7e7
김준우 <junnukim1007gmail.com@gimjun-uui-MacBookPro.local>
2025-10-17 15:08:43 +0900

    Init project

---
+++ src/main/java/com/homesweet/homesweetback/HomesweetBackApplication.java
@@ -0,0 +1,13 @@
+package com.homesweet.homesweetback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesweetBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomesweetBackApplication.class, args);
+    }
+
+}
