30f74b010504530119644c256fb4758ad45af9d0
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-10-21 11:27:25 +0900

    feat: 카테고리 생성 관련 실패 테스트 코드 작성

---
+++ src/main/java/com/homesweet/homesweetback/domain/product/category/service/ProductCategoryService.java
@@ -0,0 +1,10 @@
+package com.homesweet.homesweetback.domain.product.category.service;
+
+/**
+ * 제품 카테고리 서비스 인터페이스
+ *
+ * @author junnukim1007gmail.com
+ * @date 25. 10. 21.
+ */
+public interface ProductCategoryService {
+}
