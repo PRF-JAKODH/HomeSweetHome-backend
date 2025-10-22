321e5f33f9c3201529d51be2209aac1098fdb8d4
dogyungkim <prunsoli12@gmail.com>
2025-10-22 13:27:26 +0900

    fix: Grade 엔티티 중복 해결

--- src/main/java/com/homesweet/homesweetback/domain/grade/repository/GradeRepository.java
+++ src/main/java/com/homesweet/homesweetback/domain/grade/repository/GradeRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GradeRepository extends JpaRepository<Grade, String> {
-    // 등급 정보는 등급명으로 조회
-    Grade findByGradeName(String gradeName);
+public interface GradeRepository extends JpaRepository<Grade, Integer> {
+    // 등급 정보는 등급명으로 조회  
+    Grade findByGrade(String grade);
 
 }
