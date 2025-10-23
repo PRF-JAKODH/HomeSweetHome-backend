df6fe4f87041b9afe773846137ce086a9c4c52d4
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-10-23 19:20:38 +0900

    feat: 제품 컨트롤러 주석 처리

--- src/main/java/com/homesweet/homesweetback/domain/product/product/controller/ProductController.java
+++ src/main/java/com/homesweet/homesweetback/domain/product/product/controller/ProductController.java
@@ -1,20 +1,15 @@
 package com.homesweet.homesweetback.domain.product.product.controller;
 
-import com.homesweet.homesweetback.domain.auth.entity.OAuth2UserPrincipal;
 import com.homesweet.homesweetback.domain.product.product.controller.request.ProductSortType;
 import com.homesweet.homesweetback.domain.product.product.controller.request.ProductUploadRequest;
 import com.homesweet.homesweetback.domain.product.product.controller.response.*;
 import com.homesweet.homesweetback.domain.product.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.attribute.UserPrincipal;
 import java.util.List;
 
 /**
@@ -23,7 +18,6 @@ import java.util.List;
  * @author junnukim1007gmail.com
  * @date 25. 10. 21.
  */
-@Slf4j
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
@@ -31,6 +25,7 @@ public class ProductController {
 
     private final ProductService service;
 
+    // [판매자] - 상품 등록
     @PostMapping
     public ResponseEntity<ProductResponse> registerProduct(
             @RequestHeader(value = "X-Test-User-Id", defaultValue = "1") Long sellerId,
@@ -46,7 +41,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    // 제품 프리뷰 조회 (스토어 > 제품 조회)
+    // [모두] 제품 프리뷰 조회 (스토어 > 제품 조회)
     @GetMapping("/previews")
     public ResponseEntity<ProductScrollResponse> getProductPreviews(
             @RequestParam(required = false) Long cursorId,
@@ -60,7 +55,7 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
-    // 제품 상세 페이지 조회
+    // [모두] 제품 상세 페이지 조회
     @GetMapping("/{productId}")
     public ResponseEntity<ProductPreviewResponse> getProductDetail(@PathVariable Long productId) {
 
@@ -69,7 +64,7 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
-    // 제품 옵션 별 재고 조회 -> 무옵션, 단일 옵션, 다중 옵션마다 달라야 할까?
+    // [모두] 제품 옵션 별 재고 조회 -> 무옵션, 단일 옵션, 다중 옵션마다 달라야 할까?
     @GetMapping("/{productId}/stocks")
     public ResponseEntity<List<SkuStockResponse>> getProductStock(@PathVariable Long productId) {
         List<SkuStockResponse> response = service.getProductStock(productId);
@@ -77,9 +72,10 @@ public class ProductController {
         return ResponseEntity.ok(response);
     }
 
+    // [판매자] 판매 물품 조회
     @GetMapping
     public ResponseEntity<List<ProductManageResponse>> getSellerProducts(
-            @RequestHeader(value = "X-Test-User-Id", defaultValue = "1") Long sellerId
+            @RequestHeader(value = "X-Test-User-Id", defaultValue = "1") Long sellerId // 테스트 용
     ) {
 //        OAuth2UserPrincipal principal = (OAuth2UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 //        Long sellerId = principal.getUserId();
