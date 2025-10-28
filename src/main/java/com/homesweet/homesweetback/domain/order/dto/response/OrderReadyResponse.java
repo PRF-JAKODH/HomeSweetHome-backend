2bab468028d02137e55604a407afbcf0aa675763
chaeho5 <dkscogh1@gmail.com>
2025-10-28 14:56:59 +0900

    리팩토링 엔티티

--- src/main/java/com/homesweet/homesweetback/domain/order/dto/response/OrderReadyResponse.java
+++ src/main/java/com/homesweet/homesweetback/domain/order/dto/response/OrderReadyResponse.java
@@ -16,11 +16,8 @@ public record OrderReadyResponse(
         BigDecimal discountRate,
         Integer shippingPrice,
         Integer finalPrice, // 수량, 할인율
-        Integer quantity
-
-
-
-//        String merchantUid, // 우리가 생성한 고유 주문번호
+        Integer quantity,
+        String merchantUid // 우리가 생성한 고유 주문번호
 //        String orderName, // 결제창에 표시될 주문 이름
 //        Long totalAmount // 서버가 검증한 최종 결제 금액
 ){
