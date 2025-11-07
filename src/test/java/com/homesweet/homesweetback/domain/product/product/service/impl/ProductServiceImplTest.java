427f6ad65aa1ea4e611da4cb98a43b34dabafde1
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-07 16:05:56 +0900

    feat: 상품 프리뷰 무한 스크롤 단위 테스트 코드 작성

--- src/test/java/com/homesweet/homesweetback/domain/product/product/service/impl/ProductServiceImplTest.java
+++ src/test/java/com/homesweet/homesweetback/domain/product/product/service/impl/ProductServiceImplTest.java
@@ -1,12 +1,16 @@
 package com.homesweet.homesweetback.domain.product.product.service.impl;
 
+import com.homesweet.homesweetback.common.util.ScrollResponse;
 import com.homesweet.homesweetback.common.valid.ProductValidator;
 import com.homesweet.homesweetback.domain.product.category.domain.ProductCategory;
 import com.homesweet.homesweetback.domain.product.category.repository.ProductCategoryRepository;
+import com.homesweet.homesweetback.domain.product.product.controller.request.ProductSortType;
 import com.homesweet.homesweetback.domain.product.product.controller.request.create.ProductCreateRequest;
+import com.homesweet.homesweetback.domain.product.product.controller.response.ProductPreviewResponse;
 import com.homesweet.homesweetback.domain.product.product.controller.response.ProductResponse;
 import com.homesweet.homesweetback.domain.product.product.domain.Product;
 import com.homesweet.homesweetback.domain.product.product.domain.ProductImages;
+import com.homesweet.homesweetback.domain.product.product.domain.ProductStatus;
 import com.homesweet.homesweetback.domain.product.product.repository.ProductRepository;
 import com.homesweet.homesweetback.domain.product.product.repository.util.ProductImageUploader;
 import org.junit.jupiter.api.DisplayName;
@@ -21,6 +25,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,6 +55,26 @@ class ProductServiceImplTest {
     private ProductImageUploader productImageUploader;
     @Mock
     private ProductValidator validator;
+
+    private ProductPreviewResponse createMockProduct(Long id, String name, String brand, Integer price) {
+        return new ProductPreviewResponse(
+                id,
+                1L,
+                1L,
+                name,
+                "https://s3.aws/" + name + ".jpg",
+                brand,
+                price,
+                new BigDecimal("10.0"),
+                name + " 상세 설명",
+                3000,
+                ProductStatus.ON_SALE,
+                4.5,
+                20L,
+                LocalDateTime.now().minusDays(2),
+                LocalDateTime.now()
+        );
+    }
 
     @Nested
     @DisplayName("상품 생성")
@@ -275,30 +300,103 @@ class ProductServiceImplTest {
         @DisplayName("실패")
         class Fail {
 
-//            @Test
-//            @DisplayName("이미 등록된 상품명으로 등록할 수 없다")
-//            void createProductWithDuplicateName() {
-//                // given
-//                Long sellerId = 1L;
-//                ProductCreateRequest request = new ProductCreateRequest(
-//                        1L, "중복 상품", "홈스윗",
-//                        20000, BigDecimal.ZERO,
-//                        "중복 이름 상품", 3000,
-//                        List.of(), List.of()
-//                );
-//
-//                MockMultipartFile mainImage =
-//                        new MockMultipartFile("mainImage", "main.jpg", "image/jpeg", "data".getBytes());
-//
-//                given(productRepository.existsBySellerIdAndName(sellerId, "중복 상품")).willReturn(true);
-//
-//                // when & then
-//                assertThatThrownBy(() ->
-//                        service.registerProduct(sellerId, request, mainImage, List.of())
-//                )
-//                        .isInstanceOf(ProductException.class)
-//                        .hasMessage(ErrorCode.DUPLICATED_PRODUCT_NAME_ERROR.getMessage());
-//            }
+        }
+    }
+
+    @Nested
+    @DisplayName("상품 프리뷰 조회")
+    class FindProductReviews {
+
+        @Nested
+        @DisplayName("성공")
+        class Success {
+
+            @Test
+            @DisplayName("상품이 limit 개수보다 많은 경우 다음 페이지 존재 플래그가 true다")
+            void getProducts_hasNextPage() {
+                // given
+                Long cursorId = null;
+                Long categoryId = 1L;
+                int limit = 2;
+                String keyword = null;
+                ProductSortType sortType = ProductSortType.LATEST;
+
+                List<ProductPreviewResponse> mockProducts = List.of(
+                        createMockProduct(1L, "의자", "홈스윗", 10000),
+                        createMockProduct(2L, "테이블", "홈스윗", 20000),
+                        createMockProduct(3L, "소파", "홈스윗", 30000)
+                );
+
+                given(productRepository.findNextProducts(cursorId, categoryId, limit + 1, keyword, sortType))
+                        .willReturn(mockProducts);
+
+                // when
+                ScrollResponse<ProductPreviewResponse> response =
+                        service.getProductPreview(cursorId, categoryId, limit, keyword, sortType);
+
+                // then
+                assertThat(response.contents()).hasSize(limit);
+                assertThat(response.hasNext()).isTrue();
+                assertThat(response.nextCursorId()).isEqualTo(2L);
+            }
+
+            @Test
+            @DisplayName("상품이 limit 이하일 경우 hasNext는 false다")
+            void getProducts_noNextPage() {
+                // given
+                Long cursorId = null;
+                Long categoryId = 1L;
+                int limit = 3;
+                List<ProductPreviewResponse> mockProducts = List.of(
+                        createMockProduct(1L, "의자", "홈스윗", 10000),
+                        createMockProduct(2L, "테이블", "홈스윗", 20000)
+                );
+
+                given(productRepository.findNextProducts(cursorId, categoryId, limit + 1, null, ProductSortType.LATEST))
+                        .willReturn(mockProducts);
+
+                // when
+                ScrollResponse<ProductPreviewResponse> response =
+                        service.getProductPreview(cursorId, categoryId, limit, null, ProductSortType.LATEST);
+
+                // then
+                assertThat(response.hasNext()).isFalse();
+                assertThat(response.nextCursorId()).isNull();
+                assertThat(response.contents()).hasSize(2);
+            }
+
+            @Test
+            @DisplayName("키워드 검색과 정렬 타입이 함께 적용되어도 정상적으로 페이지네이션 된다")
+            void getProducts_withKeywordAndSortType() {
+                // given
+                Long cursorId = null;
+                Long categoryId = null;
+                int limit = 2;
+                String keyword = "테이블";
+                ProductSortType sortType = ProductSortType.POPULAR;
+
+                List<ProductPreviewResponse> mockProducts = List.of(
+                        createMockProduct(1L, "의자", "홈스윗", 10000)
+                );
+
+                given(productRepository.findNextProducts(cursorId, categoryId, limit + 1, keyword, sortType))
+                        .willReturn(mockProducts);
+
+                // when
+                ScrollResponse<ProductPreviewResponse> response =
+                        service.getProductPreview(cursorId, categoryId, limit, keyword, sortType);
+
+                // then
+                assertThat(response.contents()).hasSize(1);
+                assertThat(response.hasNext()).isFalse();
+            }
+
+        }
+
+        @Nested
+        @DisplayName("실패")
+        class Fail {
+
         }
     }
 }
