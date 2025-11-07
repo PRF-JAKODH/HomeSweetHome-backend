1e8460a3c9896a0d4f394c695ffbcb28edc26499
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-07 17:00:25 +0900

    feat:상품 업데이트 관련 단위 테스트 작성 완료

--- src/test/java/com/homesweet/homesweetback/domain/product/data/ProductFixture.java
+++ src/test/java/com/homesweet/homesweetback/domain/product/data/ProductFixture.java
@@ -1,10 +1,14 @@
 package com.homesweet.homesweetback.domain.product.data;
 
+import com.homesweet.homesweetback.domain.product.product.controller.request.update.ProductSkuUpdateRequest;
 import com.homesweet.homesweetback.domain.product.product.controller.response.ProductDetailResponse;
 import com.homesweet.homesweetback.domain.product.product.controller.response.ProductManageResponse;
 import com.homesweet.homesweetback.domain.product.product.controller.response.ProductPreviewResponse;
 import com.homesweet.homesweetback.domain.product.product.controller.response.SkuStockResponse;
+import com.homesweet.homesweetback.domain.product.product.domain.Product;
 import com.homesweet.homesweetback.domain.product.product.domain.ProductStatus;
+import com.homesweet.homesweetback.domain.product.product.domain.Sku;
+import org.springframework.mock.web.MockMultipartFile;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,8 +22,8 @@ import java.util.List;
  */
 public class ProductFixture {
 
-    // 제품 응답 DTO
-    public static ProductPreviewResponse createMockProduct(Long id, String name, String brand, Integer price) {
+    // [제품 프리뷰 조회] 제품 응답 DTO
+    public static ProductPreviewResponse createProductPreviewResponse(Long id, String name, String brand, Integer price) {
         return new ProductPreviewResponse(
                 id,
                 1L,
@@ -37,6 +41,46 @@ public class ProductFixture {
                 LocalDateTime.now().minusDays(2),
                 LocalDateTime.now()
         );
+    }
+
+    // [상품 생성] 상품명과 함께 상품 생성
+    public static Product createMockProduct(Long id, Long sellerId, String name) {
+        return Product.builder()
+                .id(id)
+                .name(name)
+                .brand("홈스윗")
+                .basePrice(10000)
+                .description("테스트 상품")
+                .shippingPrice(3000)
+                .build();
+    }
+
+    // [상품 생성] 상품 상태와 함께 상품 생성
+    public static Product createMockProduct(Long id, Long sellerId, ProductStatus status) {
+        return Product.builder()
+                .id(id)
+                .name("테스트 상품")
+                .brand("홈스윗")
+                .basePrice(10000)
+                .description("테스트 상품")
+                .shippingPrice(3000)
+                .status(status)
+                .build();
+    }
+
+    // [상품 생성] 사진과 함께 상품 생성
+    public static Product createMockProductWithImage(Long id, Long sellerId, String imageUrl) {
+        return Product.builder()
+                .id(id)
+                .sellerId(sellerId)
+                .name("테스트상품")
+                .imageUrl(imageUrl)
+                .build();
+    }
+
+    // 이미지 파일 생성
+    public static MockMultipartFile createMockFile(String name) {
+        return new MockMultipartFile(name, name + ".jpg", "image/jpeg", "data".getBytes());
     }
 
     // 제품 상세 응답 DTO
@@ -77,7 +121,8 @@ public class ProductFixture {
         );
     }
 
-    public static ProductManageResponse createMockProduct(
+    // 판매자가 본인 판매 물품 조회 응답 DTO 생성
+    public static ProductManageResponse createManageResponse(
             Long id, String name, String categoryPath,
             Integer price, BigDecimal discountRate, Long totalStock
     ) {
@@ -93,5 +138,21 @@ public class ProductFixture {
                 .status(ProductStatus.ON_SALE)
                 .createdAt(LocalDateTime.now().minusDays(3))
                 .build();
+    }
+
+    // Sku 업데이트 요청 DTO 생성
+    public static ProductSkuUpdateRequest createSkuUpdateRequest() {
+        return new ProductSkuUpdateRequest(List.of(
+                new ProductSkuUpdateRequest.SkuStockUpdateRequest(1L, 10L, 0),
+                new ProductSkuUpdateRequest.SkuStockUpdateRequest(2L, 5L, 3000)
+        ));
+    }
+
+    // SKU 생성
+    public static Sku createMockSku(Long id) {
+        Sku sku = Sku.builder()
+                .id(id)
+                .build();
+        return sku;
     }
 }
