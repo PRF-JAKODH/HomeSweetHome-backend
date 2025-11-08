685358813b413bd6ed33a7c9482160f435c9f56a
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-08 22:10:54 +0900

    feat: 장바구니 서비스 단위 테스트 작성 완료

--- src/test/java/com/homesweet/homesweetback/domain/product/category/service/impl/ProductCategoryServiceImplTest.java
+++ src/test/java/com/homesweet/homesweetback/domain/product/category/service/impl/ProductCategoryServiceImplTest.java
@@ -24,9 +24,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-
 /**
  *
  * @author junnukim1007gmail.com
