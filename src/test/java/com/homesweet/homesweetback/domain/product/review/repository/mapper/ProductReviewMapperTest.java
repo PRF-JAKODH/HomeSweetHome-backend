256ad1783af05a14ff2a8d1b13cc3961c56f86d3
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-11 16:20:39 +0900

    feat: 제품 리뷰 매핑 클래스 단위 테스트 작성 완료

---
+++ src/test/java/com/homesweet/homesweetback/domain/product/review/repository/mapper/ProductReviewMapperTest.java
@@ -0,0 +1,12 @@
+package com.homesweet.homesweetback.domain.product.review.repository.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author junnukim1007gmail.com
+ * @date 25. 11. 11.
+ */
+class ProductReviewMapperTest {
+
+}
