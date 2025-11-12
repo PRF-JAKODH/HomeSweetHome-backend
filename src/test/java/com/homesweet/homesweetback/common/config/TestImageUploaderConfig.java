796e6674ab510f9f009275017405d6be60238387
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-12 14:32:57 +0900

    feat: 이미지 업로더 테스트 코드 수정

--- src/test/java/com/homesweet/homesweetback/common/config/TestImageUploaderConfig.java
+++ src/test/java/com/homesweet/homesweetback/common/config/TestImageUploaderConfig.java
@@ -43,7 +43,7 @@ public class TestImageUploaderConfig {
         }
 
         @Override
-        public List<String> uploadFiles(List<MultipartFile> files, String directory) {
+        public List<String> uploadFiles(List<? extends MultipartFile> files, String directory) {
             if (files == null || files.isEmpty()) {
                 return new ArrayList<>();
             }
