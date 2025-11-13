9eec8f49f12c27b1f9add0f5ce4fb4acb29e35a8
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-13 15:19:26 +0900

    feat: 인증 사용자용 검색 API 구현

---
+++ src/main/java/com/homesweet/homesweetback/domain/product/product/service/impl/ProductSearchServiceImpl.java
@@ -0,0 +1,48 @@
+package com.homesweet.homesweetback.domain.product.product.service.impl;
+
+import com.homesweet.homesweetback.common.util.ScrollResponse;
+import com.homesweet.homesweetback.domain.product.product.controller.request.ProductSortType;
+import com.homesweet.homesweetback.domain.product.product.controller.response.ProductPreviewResponse;
+import com.homesweet.homesweetback.domain.product.product.repository.ProductRepository;
+import com.homesweet.homesweetback.domain.product.product.service.ProductSearchService;
+import com.homesweet.homesweetback.domain.product.product.service.RecentSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * 상품 검색 서비스 구현체
+ *
+ * @author junnukim1007gmail.com
+ * @date 25. 11. 13.
+ */
+@Service
+@RequiredArgsConstructor
+public class ProductSearchServiceImpl implements ProductSearchService {
+
+    private final ProductRepository productRepository;
+    private final RecentSearchService recentSearchService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public ScrollResponse<ProductPreviewResponse> search(Long cursorId, Long categoryId, Long userId, int limit, String keyword, ProductSortType sortType) {
+
+        recentSearchService.save(userId, keyword);
+
+        List<ProductPreviewResponse> products =
+                productRepository.findNextProducts(cursorId, categoryId, limit + 1, keyword, sortType);
+
+        boolean hasNext = products.size() > limit;
+        if (hasNext) {
+            products = products.subList(0, limit);
+        }
+
+        Long nextCursorId = hasNext
+                ? products.get(products.size() - 1).id()
+                : null;
+
+        return ScrollResponse.of(products, nextCursorId, hasNext);
+    }
+}
