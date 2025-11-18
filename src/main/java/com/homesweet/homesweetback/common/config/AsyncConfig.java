5f7d5da884e7d0a09e62c9cfab1b1fe74971ea09
dogyungkim <prunsoli12@gmail.com>
2025-11-18 19:15:11 +0900

    feat: 비동기 스레드 설정 application.yml로 관리하도록 변경

--- src/main/java/com/homesweet/homesweetback/common/config/AsyncConfig.java
+++ src/main/java/com/homesweet/homesweetback/common/config/AsyncConfig.java
@@ -1,6 +1,8 @@
 package com.homesweet.homesweetback.common.config;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -21,6 +23,13 @@ import java.util.concurrent.Executor;
 @EnableAsync
 @Profile("!test")
 public class AsyncConfig {
+
+    @Value("${notification.async.corePoolSize}")
+    private int corePoolSize;
+    @Value("${notification.async.maxPoolSize}")
+    private int maxPoolSize;
+    @Value("${notification.async.queueCapacity}")
+    private int queueCapacity;
 
     /**
      * 알림 전용 스레드 풀
