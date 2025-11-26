b4c4196524f20602e8948eab5736f63519754786
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-26 09:28:05 +0900

    refactor: 채팅방, 커뮤니티 검색 컨트롤러, 서비스 구현

---
+++ src/main/java/com/homesweet/homesweetback/domain/search/chat/service/ChatSearchService.java
@@ -0,0 +1,10 @@
+package com.homesweet.homesweetback.domain.search.chat.service;
+
+/**
+ * 단체 채팅방 검색 서비스
+ *
+ * @author junnukim1007gmail.com
+ * @date 25. 11. 26.
+ */
+public interface ChatSearchService {
+}
