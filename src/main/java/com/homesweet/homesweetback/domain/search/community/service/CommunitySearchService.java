222e54eb22bf3488fd5e40a983ba96b536599770
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-26 16:01:07 +0900

    refactor: 게시판 검색 자동완성 구현

--- src/main/java/com/homesweet/homesweetback/domain/search/community/service/CommunitySearchService.java
+++ src/main/java/com/homesweet/homesweetback/domain/search/community/service/CommunitySearchService.java
@@ -1,5 +1,7 @@
 package com.homesweet.homesweetback.domain.search.community.service;
 
+import java.util.List;
+
 /**
  * 커뮤니티 검색 서비스
  *
@@ -7,4 +9,6 @@ package com.homesweet.homesweetback.domain.search.community.service;
  * @date 25. 11. 26.
  */
 public interface CommunitySearchService {
+
+    List<String> autocomplete(String keyword);
 }
--- src/main/java/com/homesweet/homesweetback/domain/search/community/service/impl/CommunitySearchServiceImpl.java
+++ src/main/java/com/homesweet/homesweetback/domain/search/community/service/impl/CommunitySearchServiceImpl.java
@@ -1,8 +1,12 @@
 package com.homesweet.homesweetback.domain.search.community.service.impl;
 
+import com.homesweet.homesweetback.domain.search.community.repository.CommunityPostRepository;
 import com.homesweet.homesweetback.domain.search.community.service.CommunitySearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 커뮤니티 서비스 구현체
@@ -12,5 +16,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CommunitySearchServiceImpl implements CommunitySearchService {
+
+    private final CommunityPostRepository communityPostRepository;
+
+    @Override
+    public List<String> autocomplete(String keyword) {
+        return communityPostRepository.autocomplete(keyword);
+    }
 }
