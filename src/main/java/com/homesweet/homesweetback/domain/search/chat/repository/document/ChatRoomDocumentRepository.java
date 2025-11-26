010546a1371f4217f153af8adf1ff8762a7691f1
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-26 10:06:55 +0900

    refactor: ChatRoom 도큐먼트 생성

---
+++ src/main/java/com/homesweet/homesweetback/domain/search/chat/repository/document/ChatRoomDocumentRepository.java
@@ -0,0 +1,12 @@
+package com.homesweet.homesweetback.domain.search.chat.repository.document;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 채팅 엘라스틱 레포지토리
+ *
+ * @author junnukim1007gmail.com
+ * @date 25. 11. 26.
+ */
+public interface ChatRoomDocumentRepository extends ElasticsearchRepository<ChatRoomDocument, Long> {
+}
