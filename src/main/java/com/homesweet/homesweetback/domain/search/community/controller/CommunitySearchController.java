2b2d560b99ce6cc6cc8719c53d7a70b2e6df11e5
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-11-26 16:49:46 +0900

    refactor: 게시판 검색 및 조회 구현 완료

--- src/main/java/com/homesweet/homesweetback/domain/search/community/controller/CommunitySearchController.java
+++ src/main/java/com/homesweet/homesweetback/domain/search/community/controller/CommunitySearchController.java
@@ -1,9 +1,14 @@
 package com.homesweet.homesweetback.domain.search.community.controller;
 
+import com.homesweet.homesweetback.common.util.scroll.SearchScrollResponse;
+import com.homesweet.homesweetback.domain.auth.entity.OAuth2UserPrincipal;
+import com.homesweet.homesweetback.domain.search.community.controller.response.CommunityPostSearchResponse;
+import com.homesweet.homesweetback.domain.search.community.controller.response.CommunitySortType;
 import com.homesweet.homesweetback.domain.search.community.service.CommunitySearchService;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +29,9 @@ public class CommunitySearchController {
 
     private final CommunitySearchService communitySearchService;
 
+    /**
+     * 게시글 검색 자동완성
+     */
     @GetMapping("/autocomplete")
     public ResponseEntity<List<String>> autocomplete(@NotNull @RequestParam String keyword) {
 
@@ -31,4 +39,24 @@ public class CommunitySearchController {
 
         return ResponseEntity.ok(result);
     }
+
+    /**
+     * 게시글 검색 및 조회
+     */
+    @GetMapping
+    public ResponseEntity<SearchScrollResponse<CommunityPostSearchResponse>> searchPosts(
+            @RequestParam(required = false) String nextCursor,
+            @RequestParam(required = false) String keyword,
+            @RequestParam(defaultValue = "LATEST") CommunitySortType sortType,
+            @RequestParam(defaultValue = "20") int limit,
+            @AuthenticationPrincipal OAuth2UserPrincipal principal
+    ) {
+        Long userId = principal != null ? principal.getUserId() : null;
+
+        SearchScrollResponse<CommunityPostSearchResponse> response =
+                communitySearchService.search(userId, nextCursor, keyword, limit, sortType);
+
+        return ResponseEntity.ok(response);
+    }
+
 }
