822c06a101b101724ad7dd36ed34c4e81e08948a
ohhalim <ohhalim777@gmail.com>
2025-12-08 22:40:16 +0900

    refactor: N+1 쿼리 최적화
    CommunityPostRepository.findByPostIdAndIsDeletedFalse → @EntityGraph(author, author.grade) 추가
    CommunityCommentRepository.findByPost_PostIdAndIsDeletedFalse → @EntityGraph(author, author.grade) 추가

--- src/main/java/com/homesweet/homesweetback/domain/community/repository/CommunityPostRepository.java
+++ src/main/java/com/homesweet/homesweetback/domain/community/repository/CommunityPostRepository.java
@@ -13,7 +13,8 @@ import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface CommunityPostRepository extends JpaRepository<CommunityPostEntity, Long> {
-    // 특정 게시글 조회
+    // 특정 게시글 조회 - N+1 문제 해결
+    @EntityGraph(attributePaths = {"author", "author.grade"})
     Optional<CommunityPostEntity> findByPostIdAndIsDeletedFalse(Long postId);
 
     // 페이지네이션 쿼리 메서드 / N+1문제 해결위해 EntityGraph 추가
